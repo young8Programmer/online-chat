74dca2f8d67b22db8b88dc6c5094b368ae4a4878
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:37 +0500

    feat: middleware funksiyalari qo'shildi

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -2,6 +2,7 @@ import { Server, Socket } from 'socket.io';
 import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { ChatService } from './chat/chat.service';
 
+// prettier formatlash
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly chatService: ChatService) {}
