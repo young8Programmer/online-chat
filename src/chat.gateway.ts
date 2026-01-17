ade7ac8593505ea151e2979e3e337bf1ddbd414c
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:40 +0500

    feat: product catalog funksiyasi qo'shildi

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -4,6 +4,7 @@ import { Server, Socket } from 'socket.io';
 import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 // admin dashboard yaratildi
 // kod formatlash va tozalash
+// database migrations yaratildi
 import { ChatService } from './chat/chat.service';
 
 // prettier formatlash
