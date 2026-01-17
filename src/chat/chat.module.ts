485e01af9c2df898e98767bac0bed1b708ce5914
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:02 +0500

    fix: validation xatolari tuzatildi

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,6 +4,7 @@ import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatMessage } from './entities/chat.entity';
 import { ChatGateway } from 'src/chat.gateway';
+// database querylarni optimallashtirish
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChatMessage])],
