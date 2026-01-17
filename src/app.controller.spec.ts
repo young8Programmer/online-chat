52d10933d3b3f36635f2b3babe40d1f84da2f1ad
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:33:35 +0500

    test: integration testlar yaratildi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 // CI/CD pipeline sozlandi
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+// admin dashboard yaratildi
 import { AppService } from './app.service';
 
 // changelog yangilandi
