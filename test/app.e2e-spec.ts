31481ad59b5d4817e2265476baed55206ae72835
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:49 +0500

    test: component testlari yaratildi

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+// ESLint qoidalariga moslashtirish
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
