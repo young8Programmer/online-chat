72af81a18842f3f0876d292c83071e9581f28fa3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:33:48 +0500

    perf: database querylarni optimallashtirish

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
 // componentlarni qayta tashkilash
 // image optimization qo'shildi
 // kod uslubini yaxshilash
+// routing muammosi hal qilindi
     return 'Hello World!';
 // package.json yangilandi
   }
