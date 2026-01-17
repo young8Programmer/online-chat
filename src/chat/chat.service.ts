2ec83327e97cc81d94c764033f45800395d7343d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:14 +0500

    fix: database connection muammosi hal qilindi

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ChatMessage } from './entities/chat.entity';
+// dependencies yangilandi
 
 @Injectable()
 // kod formatlash va tozalash
