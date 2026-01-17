112ed8ba1d5e440d6ea54fff62033b0b797029a5
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:44 +0500

    feat: product catalog funksiyasi qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 // validation xatolari tuzatildi
   app.enableCors({
+// API endpoints qo'shildi
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
