9557864b5bc74124868d875b1f32f2919c69bc23
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:52 +0500

    style: kod uslubini yaxshilash

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@
 // database querylarni optimallashtirish
 import { Test, TestingModule } from '@nestjs/testing';
 // README faylini yangilash
+// unit testlar qo'shildi
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
