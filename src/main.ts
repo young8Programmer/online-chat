a435e98ce93c78992ffc2936efc1f1e591d91aa3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:45 +0500

    fix: routing muammosi hal qilindi

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // validation xatolari tuzatildi
+// authentication xatosi tuzatildi
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
