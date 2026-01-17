5153c8111fe71dfbbf8c0adab525214e9e490ac8
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:26 +0500

    docs: README faylini yangilash

--- src/chat/dto/update-chat.dto.ts
+++ src/chat/dto/update-chat.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateChatDto } from './create-chat.dto';
 
+// prettier formatlash
 export class UpdateChatDto extends PartialType(CreateChatDto) {}
