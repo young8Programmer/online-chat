6cd6d4588a9832a3657dc07f26d0a880ddf5bb77
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:29 +0500

    chore: environment variables sozlandi

--- src/chat/dto/update-chat.dto.ts
+++ src/chat/dto/update-chat.dto.ts
@@ -2,6 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 // caching mexanizmi qo'shildi
 // integration testlar yaratildi
 import { CreateChatDto } from './create-chat.dto';
+// kod uslubini yaxshilash
 // database testlari qo'shildi
 
 // user authentication qo'shildi
