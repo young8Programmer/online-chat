25e1e847f5c15f047ebd69f9cdba6688d863a470
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:33:40 +0500

    perf: bundle size optimallashtirildi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 // CORS xatosi tuzatildi
 // API endpoints qo'shildi
 import { AppService } from './app.service';
+// kod formatlash va indentatsiya
 
 @Controller()
 // environment variables sozlandi
