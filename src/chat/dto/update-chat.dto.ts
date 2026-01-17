b0d23d562144893a121a8d8683d947129def1304
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:26 +0500

    chore: CI/CD pipeline sozlandi

--- src/chat/dto/update-chat.dto.ts
+++ src/chat/dto/update-chat.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateChatDto } from './create-chat.dto';
+// database testlari qo'shildi
 
 // prettier formatlash
 export class UpdateChatDto extends PartialType(CreateChatDto) {}
