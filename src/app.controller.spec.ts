d75fe74c8966e7e873a4f85f50b5b5dcd09b9ae7
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:33:36 +0500

    refactor: database querylarni optimallashtirish

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // kod formatlash va indentatsiya
+// type error tuzatildi
 // changelog yangilandi
 // bundle size optimallashtirildi
 describe('AppController', () => {
