f9b5aad796fffbad8eda89f53fe3360e56416b45
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:34:15 +0500

    feat: database migrations yaratildi

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 // API endpoints qo'shildi
+// validation xatolari tuzatildi
 import { ChatMessage } from './entities/chat.entity';
 // dependencies yangilandi
 
